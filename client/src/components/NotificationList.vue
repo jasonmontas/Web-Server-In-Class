403f31ee158711f33acc059b6ed4c1d70e1a7186
jasonmontas <jason.montas24@gmail.com>
2025-04-24 14:44:56 -0400

    fixed notificationlist

--- client/src/components/NotificationList.vue
+++ client/src/components/NotificationList.vue
@@ -1,22 +1,20 @@
-<script setup lang="js">
-import { refNotifications, removeNotification } from '@/models/notifications'
-import { ref } from 'vue'
+<script setup lang="ts">
+import { refNotifications, removeNotification } from '@/models/notifications';
+import { ref } from 'vue';
 
 const notifications = refNotifications()
+
+
 </script>
 
 <template>
-  <div>
-    <div
-      v-for="(message, index) in notifications"
-      :key="index"
-      class="notification is-light"
-      :class="'is-' + message.type"
-    >
-      <button class="delete" @click="removeNotification(index)"></button>
-      {{ message.message }}
+    <div>
+        <div v-for="(message, index) in notifications" :key="index" class="notification is-light"
+             :class="`is-${message.type}`">
+            <button class="delete" @click="removeNotification(index)"></button>
+            {{ message.message }}
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped></style>
